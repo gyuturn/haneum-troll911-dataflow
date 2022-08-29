8022b0149304df6b42c33a75ee05c25807956026
turn <kksy2423@naver.com>
2022-08-30 00:11:42 +0900

    feat: api 호출 설정

---
+++ src/main/java/haneum/troller/dataflow/common/env/ApiEnv.java
@@ -0,0 +1,9 @@
+package haneum.troller.dataflow.common.env;
+
+import lombok.Getter;
+
+@Getter
+public class ApiEnv {
+//    public static final String URL = "http://3.37.22.89:8080/api";
+    public static final String URL = "http://localhost:8080/api";
+}
