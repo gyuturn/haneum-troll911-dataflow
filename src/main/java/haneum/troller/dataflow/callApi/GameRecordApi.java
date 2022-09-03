86e97958ab239753cfbf272f5de911b0d4f52a54
gyuturn <rbtjrrbals@gmail.com>
2022-09-03 22:55:17 +0900

    feat: gamerecord entity

---
+++ src/main/java/haneum/troller/dataflow/callApi/GameRecordApi.java
@@ -0,0 +1,12 @@
+package haneum.troller.dataflow.callApi;
+
+public class GameRecordApi {
+
+    public void twentyGameRecord(){
+        String addUrl = "/search/user/gameRecord";
+
+
+
+    }
+
+}
--- src/main/java/haneum/troller/dataflow/common/env/ApiEnv.java
+++ src/main/java/haneum/troller/dataflow/common/env/ApiEnv.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class ApiEnv {
-//    public static final String URL = "http://3.37.22.89:8080/api";
-    public static final String URL = "http://localhost:8080/api";
+    public static final String URL = "http://3.37.22.89:8080/api";
+//    public static final String URL = "http://localhost:8080/api";
 }
