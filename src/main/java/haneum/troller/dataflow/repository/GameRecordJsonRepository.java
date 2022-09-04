4981d370854912faf2daa47222dfb6d86cc9e00d
turn <kksy2423@naver.com>
2022-09-04 23:58:46 +0900

    docs: 추후 구현시 다시 사용예정

--- src/main/java/haneum/troller/dataflow/repository/GameRecordJsonRepository.java
+++ src/main/java/haneum/troller/dataflow/repository/GameRecordJsonRepository.java
@@ -1,7 +1,7 @@
-package haneum.troller.dataflow.repository;
-
-import haneum.troller.dataflow.domain.GameRecordJson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRecordJsonRepository extends JpaRepository<GameRecordJson,Long> {
-}
+//package haneum.troller.dataflow.repository;
+//
+//import haneum.troller.dataflow.domain.GameRecordJson;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface GameRecordJsonRepository extends JpaRepository<GameRecordJson,Long> {
+//}
