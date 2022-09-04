90ad46bcea4b2ce7e0ca2f1dc06ff3c05e6c4a6c
turn <kksy2423@naver.com>
2022-09-04 23:59:18 +0900

    docs: 추후 구현시 다시 사용예정

--- src/main/java/haneum/troller/dataflow/callApi/GameRecordApi.java
+++ src/main/java/haneum/troller/dataflow/callApi/GameRecordApi.java
@@ -1,12 +1,12 @@
-package haneum.troller.dataflow.callApi;
-
-public class GameRecordApi {
-
-    public void twentyGameRecord(){
-        String addUrl = "/search/user/gameRecord";
-
-
-
-    }
-
-}
+//package haneum.troller.dataflow.callApi;
+//
+//public class GameRecordApi {
+//
+//    public void twentyGameRecord(){
+//        String addUrl = "/search/user/gameRecord";
+//
+//
+//
+//    }
+//
+//}
--- src/main/java/haneum/troller/dataflow/callApi/MemberApi.java
+++ src/main/java/haneum/troller/dataflow/callApi/MemberApi.java
@@ -1,36 +1,49 @@
-package haneum.troller.dataflow.callApi;
-
-import com.google.gson.Gson;
-import haneum.troller.dataflow.common.Apimethod.CallApi;
-import haneum.troller.dataflow.dto.DateDto;
-import org.json.simple.JSONObject;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
-import java.time.LocalDateTime;
-
-
-public class MemberApi {
-    String addUrl = "/dataflow/compare/member/date";
-
-    public void CallMemberListDateBefore() {
-        LocalDateTime now = LocalDateTime.now();
-
-
-        Gson gson = new Gson();
-        String jsonToString = gson.toJson(DateDto.builder()
-                .year(now.getYear())
-                .month(now.getMonthValue())
-                .day(now.getDayOfMonth())
-                .hour(now.getHour())
-                .minute(now.getMinute())
-                .second(now.getSecond())
-                .build());
-
-        ResponseEntity responseEntity = CallApi.PostIncludeObject(addUrl, jsonToString);
-        System.out.println(responseEntity.getBody());
-    }
-
-}
-
+//package haneum.troller.dataflow.callApi;
+//
+//import com.google.gson.Gson;
+//import haneum.troller.dataflow.common.Apimethod.CallApi;
+//import haneum.troller.dataflow.dto.DateDto;
+//import org.json.simple.JSONObject;
+//import org.springframework.http.*;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.net.URI;
+//import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//
+//public class MemberApi {
+//    String addUrl = "/dataflow/compare/member/date";
+//
+//    public List<Long> CallMemberListDateBefore() {
+//        LocalDateTime now = LocalDateTime.now();
+//
+//
+//        Gson gson = new Gson();
+//        String jsonToString = gson.toJson(DateDto.builder()
+//                .year(now.getYear())
+//                .month(now.getMonthValue())
+//                .day(now.getDayOfMonth())
+//                .hour(now.getHour())
+//                .minute(now.getMinute())
+//                .second(now.getSecond())
+//                .build());
+//
+//        ResponseEntity responseEntity = CallApi.PostIncludeObject(addUrl, jsonToString);
+//
+//        List<Long> membersId = new ArrayList<Long>();
+//        System.out.println(responseEntity.getBody());
+//        List<String> myList = new ArrayList<String>(Arrays.asList(responseEntity.getBody().toString().split(",")));
+//
+//        for (String s : myList) {
+//            membersId.add(Long.valueOf(s));
+//        }
+//
+//        System.out.println("membersId = " + membersId);
+//        return membersId;
+//    }
+//
+//}
+//
