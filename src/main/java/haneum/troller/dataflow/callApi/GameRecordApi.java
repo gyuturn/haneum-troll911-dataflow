dbef2e7f46bcd4ec42379e44c319658469bf3889
turn <kksy2423@naver.com>
2022-09-07 23:30:21 +0900

    feat: call api

--- src/main/java/haneum/troller/dataflow/callApi/GameRecordApi.java
+++ src/main/java/haneum/troller/dataflow/callApi/GameRecordApi.java
@@ -1,12 +1,14 @@
-//package haneum.troller.dataflow.callApi;
-//
-//public class GameRecordApi {
-//
-//    public void twentyGameRecord(){
-//        String addUrl = "/search/user/gameRecord";
-//
-//
-//
-//    }
-//
-//}
+package haneum.troller.dataflow.callApi;
+
+import haneum.troller.dataflow.common.Apimethod.CallApi;
+import org.springframework.http.ResponseEntity;
+
+public class GameRecordApi {
+
+    public static String fullRecordGameRecord(String lolName){
+        String addUrl = "/search/user/gameRecord"+"?lolName="+lolName;
+        ResponseEntity responseEntity = CallApi.GetIncludeParameter(addUrl);
+        return responseEntity.getBody().toString();
+    }
+
+}
